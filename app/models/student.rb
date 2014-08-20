5da576ed6491936a470b87ae45770c3dabd54851
Tim Licata <tim@timothylicata.com>
2014-08-19 21:41:38 -0700

    store regex in variable

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ActiveRecord::Base
   validates :first_name, length: {:minimum => 4}, presence: true
   validates :last_name, length: {:minimum => 4}, presence: true, uniqueness: true
 
-  validates_format_of :first_name, :with => /\A[A-Z][a-zA-Z]*\z/
-  validates_format_of :last_name, :with => /\A[A-Z][a-zA-Z]*\z/
+  @only_letters_and_start_with_cap = /\A[A-Z][a-zA-Z]*\z/
+  validates_format_of :first_name, :with => @only_letters_and_start_with_cap
+  validates_format_of :last_name, :with => @only_letters_and_start_with_cap
 end
