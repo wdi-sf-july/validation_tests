dd6e0df87039c8dce65ce6ecf1f1e8325a0a1835
Tim Licata <tim@timothylicata.com>
2014-08-21 22:42:02 -0700

    test 1) create student with new syntax

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "1) should create a student with the new syntax" do
+    tim = Student.new(:first_name => "Tim", :last_name => "Licata", :age => 30)
+    expect(tim.class).to eq(Student)
+    expect(tim.first_name).to eq("Tim")
+    expect(tim.last_name).to eq("Licata")
+    expect(tim.age).to eq(30)
+    expect(tim.new_record?).to eq(true)
+  end
 end
